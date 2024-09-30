eae5372156f03ab6a88f9f47963924de21c00b8e
haachamatcha <re.jaybel@gmail.com>
2024-09-30 15:20:08 -0400

    Router

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 // ONLY USE COMPOSITION API MEANING USE SCRIPT SETUP
 import { RouterLink, RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
   <header>
+    <NavBar />
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
