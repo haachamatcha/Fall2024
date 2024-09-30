68c548f420bc08fea26ed9820a95f107f80ddb51
haachamatcha <re.jaybel@gmail.com>
2024-09-30 14:57:56 -0400

    vue removals

--- Client/src/views/HomeView.vue
+++ Client/src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Home</h1>
+    <h1>사랑해</h1>
   </main>
 </template>
