37453f3e9394e09c080feb34ddac089ab79826bb
haachamatcha <re.jaybel@gmail.com>
2024-11-04 15:02:24 -0500

    controllers added

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
+const userController = require("./controllers/users");
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app
+  .get("/", (req, res) => {
+    res.send("The World Hasn't Ended Yet.");
+  })
+  .get("/about", (req, res) => {
+    res.send("About Us.");
+  })
+  .use("/users", userController);
 
 app.listen(PORT, () => {
   console.log("The World Hasn't Ended Yet.");
-  console.log("I love you" + PORT);
+  console.log("I love you " + PORT);
 });
